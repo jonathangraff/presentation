47d31332658d595fb50792c2b09d7fdecc0f70ac
jonathangraff <jonathan.graff@hotmail.fr>
2021-10-30 18:51:34 +0200

    ajout de projets

--- menu.js
+++ menu.js
@@ -1,1 +1,12 @@
-document.write("<nav><ul class = menu1><li><a href=index.html>Accueil</a></li><li><a href=portfolio.html>Portfolio </a><ul class='sous'><li><a href=portfolio-site-restaurant.html>Site web d'un restaurant</a></li><li><a href=portfolio-galaxy.html>Jeu mobile \"Galaxy\"</a></li><li><a href=portfolio-MrBeat.html>Boîte à rythmes \"MrBeat\"</a></li></ul></li><li><a href=CV.html>CV</a></li><li><a href=Contact.html>Contact</a></li></ul></nav>");  
+document.write("<nav><ul class = menu1><li><a href=index.html>Accueil</a></li>")
+document.write("<li><a href=portfolio.html>Portfolio </a>")
+
+document.write("<ul class='sous'><li><a href=portfolio-site-restaurant.html>Site web d'un restaurant</a>")
+document.write("</li><li><a href=portfolio-galaxy.html>Jeu mobile \"Galaxy\"</a></li>")
+document.write("<li><a href=portfolio-MrBeat.html>Boîte à rythmes \"MrBeat\"</a></li>")
+document.write("<li><a href=portfolio-MrBeat.html>Programmation de bras de robots en Coq</a></li>")
+document.write("<li><a href=portfolio-MrBeat.html>Apprentissage par renforcement profond </a></li>")
+document.write("<li><a href=portfolio-MrBeat.html>Implémentation de surfaces minimales en TypeScript </a></li>")
+
+document.write("</ul></li><li><a href=CV.html>CV</a></li>")
+document.write("<li><a href=Contact.html>Contact</a></li></ul></nav>");  
---
+++ portfolio.js
@@ -0,0 +1,35 @@
+document.write("<table class='col-portfolio' style='display:inline-block'><tr><td><div class='barre-verticale'></div></td>")
+
+document.write("<td style='width:100%;'><a href='portfolio-site-restaurant.html'><img style='width:100%' src='images/project1small.jpg'>")
+document.write("</a></td></tr><tr><td></td>")
+document.write("<td align='center'><a href='portfolio-site-restaurant.html'>Site web pour un restaurant</a></td></tr></table>")
+
+document.write("<table  class='col-portfolio float-right'><tr><td><div class='barre-verticale'></div></td>")
+
+document.write("<td style='width:100%;'><a href='portfolio-galaxy.html'><img style='width:100%' src='images/galaxy.jpg'>")
+document.write("</a></td></tr><tr><td></td>")
+document.write("<td align='center'><a href='portfolio-application-mobile-ai.html'>Jeu mobile 'Galaxy'</a></td></tr></table>")
+
+document.write("<table class='col-portfolio' style='display:inline-block'><tr><td><div class='barre-verticale'></div></td>")
+
+document.write("<td style='width:100%;'><a href='portfolio-site-restaurant.html'><img style='width:100%' src='images/mrbeat.jpg'>")
+document.write("</a></td></tr><tr><td></td>")
+document.write("<td align='center'><a href='portfolio-site-restaurant.html'>Création d'une boîte à rythmes<br><br></a></td></tr></table>")
+
+document.write("<table  class='col-portfolio float-right'><tr><td><div class='barre-verticale'></div></td>")
+
+document.write("<td style='width:100%;'><a href='portfolio-galaxy.html'><img style='width:100%' src='images/coq.png'>")
+document.write("</a></td></tr><tr><td></td>")
+document.write("<td align='center'><a href='portfolio-application-mobile-ai.html'>Stage M2 - Programmation en Coq de bras de robots</a></td></tr></table>")
+
+document.write("<table class='col-portfolio' style='display:inline-block'><tr> <td><div class='barre-verticale'></div></td>")
+
+document.write("<td style='width:100%;'><a href='portfolio-site-restaurant.html'><img style='width:100%' src='images/renforcement.jpg'>")
+document.write("</a></td></tr><tr><td></td>")
+document.write("<td align='center'><a href='portfolio-site-restaurant.html'>Projet M2 - Apprentissage par renforcement profond appliqué à des problèmes combinatoires</a></td></tr></table>")
+
+document.write("<table  class='col-portfolio float-right'><tr><td><div class='barre-verticale'></div></td>")
+
+document.write("<td style='width:100%;'><a href='portfolio-galaxy.html'><img style='width:100%' src='images/catenoide.gif'>")
+document.write("</a></td></tr><tr><td></td>")
+document.write("<td align='center'><a href='portfolio-application-mobile-ai.html'>Stage M1 - Implémentation de surfaces minimales en TypeScript</a></td></tr></table>")
