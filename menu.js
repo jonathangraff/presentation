0826a11c6a917cb681c0745ee0c447f9495cfd63
jonathangraff <jonathan.graff@hotmail.fr>
2021-10-31 23:23:07 +0100

    version 1.0

--- menu.js
+++ menu.js
@@ -4,9 +4,9 @@ document.write("<li><a href=portfolio.html>Portfolio </a>")
 document.write("<ul class='sous'><li><a href=portfolio-site-restaurant.html>Site web d'un restaurant</a>")
 document.write("</li><li><a href=portfolio-galaxy.html>Jeu mobile \"Galaxy\"</a></li>")
 document.write("<li><a href=portfolio-MrBeat.html>Boîte à rythmes \"MrBeat\"</a></li>")
-document.write("<li><a href=portfolio-MrBeat.html>Programmation de bras de robots en Coq</a></li>")
-document.write("<li><a href=portfolio-MrBeat.html>Apprentissage par renforcement profond </a></li>")
-document.write("<li><a href=portfolio-MrBeat.html>Implémentation de surfaces minimales en TypeScript </a></li>")
+document.write("<li><a href=portfolio-stageM2.html>Programmation de bras de robots en Coq</a></li>")
+document.write("<li><a href=portfolio-projetM2.html>Apprentissage par renforcement profond </a></li>")
+document.write("<li><a href=portfolio-stageM1.html>Implémentation de surfaces minimales en TypeScript </a></li>")
 
 document.write("</ul></li><li><a href=CV.html>CV</a></li>")
 document.write("<li><a href=Contact.html>Contact</a></li></ul></nav>");  
--- portfolio.js
+++ portfolio.js
@@ -20,16 +20,16 @@ document.write("<table  class='col-portfolio float-right'><tr><td><div class='ba
 
 document.write("<td style='width:100%;'><a href='portfolio-galaxy.html'><img style='width:100%' src='images/coq.png'>")
 document.write("</a></td></tr><tr><td></td>")
-document.write("<td align='center'><a href='portfolio-application-mobile-ai.html'>Stage M2 - Programmation en Coq de bras de robots</a></td></tr></table>")
+document.write("<td align='center'><a href='portfolio-stageM2.html'>Stage M2 - Programmation en Coq de bras de robots</a></td></tr></table>")
 
 document.write("<table class='col-portfolio' style='display:inline-block'><tr> <td><div class='barre-verticale'></div></td>")
 
 document.write("<td style='width:100%;'><a href='portfolio-site-restaurant.html'><img style='width:100%' src='images/renforcement.jpg'>")
 document.write("</a></td></tr><tr><td></td>")
-document.write("<td align='center'><a href='portfolio-site-restaurant.html'>Projet M2 - Apprentissage par renforcement profond appliqué à des problèmes combinatoires</a></td></tr></table>")
+document.write("<td align='center'><a href='portfolio-projetM2.html'>Projet M2 - Apprentissage par renforcement profond appliqué à des problèmes combinatoires</a></td></tr></table>")
 
 document.write("<table  class='col-portfolio float-right'><tr><td><div class='barre-verticale'></div></td>")
 
 document.write("<td style='width:100%;'><a href='portfolio-galaxy.html'><img style='width:100%' src='images/catenoide.gif'>")
 document.write("</a></td></tr><tr><td></td>")
-document.write("<td align='center'><a href='portfolio-application-mobile-ai.html'>Stage M1 - Implémentation de surfaces minimales en TypeScript</a></td></tr></table>")
+document.write("<td align='center'><a href='portfolio-stageM1.html'>Stage M1 - Implémentation de surfaces minimales en TypeScript</a></td></tr></table>")
