b70321df9af36914f6cef07b5a6c932d63f8875b
jonathangraff <jonathan.graff@hotmail.fr>
2021-12-24 17:39:25 +0100

    lien mr beat cassé

--- menu.js
+++ menu.js
@@ -3,7 +3,7 @@ document.write("<li><a href=portfolio.html>Portfolio </a>")
 
 document.write("<ul class='sous'><li><a href=portfolio-site-restaurant.html>Site web d'un restaurant</a>")
 document.write("</li><li><a href=portfolio-galaxy.html>Jeu mobile \"Galaxy\"</a></li>")
-document.write("<li><a href=portfolio-MrBeat.html>Boîte à rythmes \"MrBeat\"</a></li>")
+document.write("<li><a href=portfolio-mrbeat.html>Boîte à rythmes \"MrBeat\"</a></li>")
 document.write("<li><a href=portfolio-stageM2.html>Programmation de bras de robots en Coq</a></li>")
 document.write("<li><a href=portfolio-projetM2.html>Apprentissage par renforcement profond </a></li>")
 document.write("<li><a href=portfolio-stageM1.html>Implémentation de surfaces minimales en TypeScript </a></li>")
